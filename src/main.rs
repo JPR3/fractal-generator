e27a259e393736d14861c5bdd12173bc411958ac
JPR3 <jpr3@riganati.com>
2023-10-02 22:05:09 -0400

    Added command line input to determine number of sides

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,19 @@ use fractal_generator::generator_logic;
 use fractal_generator::rend::Renderer;
 use fractal_generator::WINDOW_HEIGHT;
 use fractal_generator::WINDOW_WIDTH;
-
+use std::env;
 //TODO accept argument
 pub fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let sides = match args.len() {
+        1 => 3,
+        2 => match args[1].parse::<i32>() {
+            Ok(n) => n,
+            Err(_) => panic!("Invlaid argument - enter an integer value"),
+        },
+        _ => panic!("Invlaid arguments - enter one integer value"),
+    };
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
     let window = video_subsystem
@@ -15,7 +25,7 @@ pub fn main() -> Result<(), String> {
         .build()
         .map_err(|e| e.to_string())?;
 
-    let vertex_coords = fractal_generator::calculate_vertices(3);
+    let vertex_coords = fractal_generator::calculate_vertices(sides);
     let renderer = Renderer::new(window, &vertex_coords)?;
     generator_logic::generate(sdl_context, renderer, vertex_coords)?;
     Ok(())
